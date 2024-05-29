6dd97dff774482fa884cb3e57615c5e4182f1924
yechezkel chen <yl9753@gmail.com>
2024-05-29 20:15:40 +0300

    Add main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-// import { useState } from 'react'
-//import './App.css'
-//import participants from ''
+import React, { useState } from 'react';
+import './App.css'
 import TextEditor from './text_editor/TextEditor.jsx'
 import Game from './game/Game.jsx'
 
-// import Buttons from "./game/ButtonsGame.jsx";
-
-// const apps = {game: <Game />};
-
 function App() {
+  const [showTextEditor, setShowTextEditor] = useState(false);
+  const [showGame, setShowGame] = useState(false);
+
+  const handleTextEditorClick = () => {
+    setShowTextEditor(true);
+    setShowGame(false);
+  };
+
+  const handleGameClick = () => {
+    setShowTextEditor(false);
+    setShowGame(true);
+  };
+
   return (
-    <>
-      <TextEditor />
-      {/* <Game /> */}
-    </>
+    <div className="main-container">
+      <h1>Main Page</h1>
+      <div className="button-container">
+        <button className="app-button" onClick={handleTextEditorClick}>Text Editor</button>
+        <button className="app-button" onClick={handleGameClick}>Game</button>
+      </div>
+
+      {showTextEditor && <TextEditor />}
+      {showGame && <Game />}
+    </ div>
   )
 }
 
